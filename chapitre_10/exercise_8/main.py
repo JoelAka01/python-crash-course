53dc515a502bdb4fae3dc384f2ccf5a5e51e390d
joelAka01 <joelemanuel12345@gmail.com>
2026-09-03 10:26:49 +0200

    Chapitre 10 exercise 8

---
+++ chapitre_10/exercise_8/main.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+fichiers = ['dogs.txt', 'cats.txt']
+
+for nom_fichier in fichiers:
+    chemin = Path(nom_fichier)
+    try :
+        donnees = chemin.read_text(encoding='utf-8')
+    except FileNotFoundError:
+        print(f"Alerte : Le fichier '{nom_fichier}' est manquant. Étape ignorée.")
+    else:
+        print(f"Succès : {nom_fichier}")
